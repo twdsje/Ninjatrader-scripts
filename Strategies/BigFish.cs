6fe69dc5a69994e31abde873b03dec95d7405ad4
EXTENDHEALTH\setell <seth.ellis3@extendhealth.com>
2016-12-31 22:30:53 -0700

    Code cleanup.  Profitable strategy backtesting over year.

--- Strategies/BigFish.cs
+++ Strategies/BigFish.cs
@@ -62,15 +62,15 @@ namespace NinjaTrader.NinjaScript.Strategies
 				NumberOfFish								= 5000;
 				BigFishSize									= 100;
 				StopLoss 									= 5;
+				UseTrailingStop								= false;
 				ProfitTarget								= 15;
 				MovingAveragePeriod							= 50;
 			}
 			else if (State == State.Configure)
 			{
-
-				
-				//SetTrailStop(CalculationMode.Ticks, 15);
 				SetStopLoss(CalculationMode.Ticks, StopLoss);
+				if(UseTrailingStop)
+					SetTrailStop(CalculationMode.Ticks, StopLoss);
 				SetProfitTarget(CalculationMode.Ticks, ProfitTarget);
 				Whales = SizeFilteredBuySellVolume(BigFishSize);
 				AllFish = SizeFilteredBuySellVolume(1);		
@@ -90,74 +90,118 @@ namespace NinjaTrader.NinjaScript.Strategies
 			{
         		return;
 			}
-			
-			double BigBuys = Whales.Buys[0];
-			double BigSells = Whales.Sells[0];
-			double AllBuys = AllFish.Buys[0];
-			double AllSells = AllFish.Sells[0];
-		    double NormalBuys = AllBuys - BigBuys;
-			double NormalSells = AllSells - BigSells;
-			
-			if(BarsInProgress == 1)
-			{
-				Print("BarsInProgress is 1");
-				
-				//Trend = PriceActionSwingOscillator(BarsArray[1], SwingStyle.Standard, 7, 20, false, true, true);
-				//double buyStrength = Trend.SwingLow[0];
-				//double sellStrength = Trend.RTDownTrend[0];
-			
-				//Print(buyStrength.ToString());
-			}
 
 			
-			if(Trend[0] < Close[0])
+			if(Trend[0] > Close[0])
 			{					
-				if(AllBuys > NumberOfFish && AllBuys > AllSells * 2 && BigBuys != 0 && BigSells == 0)
-				{
-					Print("Uptrend");
-					
-					EnterLong(BigBuys + "/" + BigSells + "!" + AllBuys + "/" + AllSells);
-				}
-				
-				if(AllSells > NumberOfFish && AllSells < AllBuys * 1.25)
-				{
-					ExitLong(AllSells.ToString());
-				}
-				
-				if(AllSells > NumberOfFish && AllSells > AllBuys * 2 && BigSells != 0 && BigBuys == 0)
-				{				
-					ExitLong(AllSells.ToString());
-				}
-				
-				if(AllBuys > NumberOfFish && AllBuys < AllSells * 1.25)
-				{
-					ExitLong(AllSells.ToString());
-				}				
+				FadeLongs();		
 			}
 			else
 			{
+				//FadeShorts(); //48.22
+				//FollowLongs(); 8
+				//FollowShorts();
+			}
+		}
+		
+		
+		private void FadeLongs()
+		{							
+			if(AllBuys > NumberOfFish && AllBuys > AllSells * 2 && BigBuys != 0 && BigSells == 0)
+			{
+				Print("Uptrend");
 				
-				if(AllBuys > NumberOfFish && AllBuys > AllSells * 2 && BigBuys != 0 && BigSells == 0)
-				{
-					ExitShort(BigBuys + "/" + BigSells + "!" + AllBuys + "/" + AllSells);
-				}
+				EnterShort(AllBuys.ToString());
+			}
+			
+			if(AllSells > NumberOfFish && AllSells < AllBuys * 1.25)
+			{
+				ExitShort(AllSells.ToString());
+			}
+			
+			if(AllSells > NumberOfFish && AllSells > AllBuys * 2 && BigSells != 0 && BigBuys == 0)
+			{				
+				ExitShort(AllSells.ToString());
+			}
+			
+			if(AllBuys > NumberOfFish && AllBuys < AllSells * 1.25)
+			{
+				ExitShort(AllSells.ToString());
+			}				
+		}
+		
+		private void FollowLongs()
+		{						
+			if(AllBuys > NumberOfFish && AllBuys > AllSells * 2 && BigBuys != 0 && BigSells == 0)
+			{
+				Print("Uptrend");
 				
-				if(AllSells > NumberOfFish && AllSells < AllBuys * 1.25)
-				{
-					ExitShort(AllSells.ToString());
-				}
-				
-				if(AllSells > NumberOfFish && AllSells > AllBuys * 2 && BigSells != 0 && BigBuys == 0)
-				{
-					Print("Downtrend");				
-				
-					EnterShort(AllSells.ToString());
-				}
-				
-				if(AllBuys > NumberOfFish && AllBuys < AllSells * 1.25)
-				{
-					ExitShort(AllSells.ToString());
-				}
+				EnterLong(AllBuys.ToString());
+			}
+			
+			if(AllSells > NumberOfFish && AllSells < AllBuys * 1.25)
+			{
+				ExitLong(AllSells.ToString());
+			}
+			
+			if(AllSells > NumberOfFish && AllSells > AllBuys * 2 && BigSells != 0 && BigBuys == 0)
+			{				
+				ExitLong(AllSells.ToString());
+			}
+			
+			if(AllBuys > NumberOfFish && AllBuys < AllSells * 1.25)
+			{
+				ExitLong(AllSells.ToString());
+			}				
+		}
+		
+		private void FadeShorts()
+		{				
+			if(AllBuys > NumberOfFish && AllBuys > AllSells * 2 && BigBuys != 0 && BigSells == 0)
+			{
+				ExitLong(BigBuys + "/" + BigSells + "!" + AllBuys + "/" + AllSells);
+			}
+			
+			if(AllSells > NumberOfFish && AllSells < AllBuys * 1.25)
+			{
+				ExitLong(AllSells.ToString());
+			}
+			
+			if(AllSells > NumberOfFish && AllSells > AllBuys * 2 && BigSells != 0 && BigBuys == 0)
+			{
+				Print("Downtrend");				
+			
+				EnterLong(AllSells.ToString());
+			}
+			
+			if(AllBuys > NumberOfFish && AllBuys < AllSells * 1.25)
+			{
+				ExitLong(AllSells.ToString());
+			}
+		}
+		
+		private void FollowShorts()
+		{		
+			if(AllBuys > NumberOfFish && AllBuys > AllSells * 2 && BigBuys != 0 && BigSells == 0)
+			{
+				ExitShort(BigBuys + "/" + BigSells + "!" + AllBuys + "/" + AllSells);
+			}
+			
+			if(AllSells > NumberOfFish && AllSells < AllBuys * 1.25)
+			{
+				ExitShort(AllSells.ToString());
+			}
+			
+			if(AllSells > NumberOfFish && AllSells > AllBuys * 2 && BigSells != 0 && BigBuys == 0)
+			{
+				Print("Downtrend");				
+			
+				EnterShort(AllSells.ToString());
+			}
+			
+			if(AllBuys > NumberOfFish && AllBuys < AllSells * 1.25)
+			{
+				ExitShort(AllSells.ToString());
 			}
 		}
 
@@ -186,12 +230,46 @@ namespace NinjaTrader.NinjaScript.Strategies
 		public int StopLoss
 		{ get; set; }
 
+		[NinjaScriptProperty]
+		[Display(ResourceType = typeof(Custom.Resource), Name="Use Trailing Stop Loss", Order=3, GroupName="NinjaScriptStrategyParameters")]
+		public bool UseTrailingStop
+		{ get; set; }
 		
 		[NinjaScriptProperty]
 		[Range(1, int.MaxValue)]
 		[Display(ResourceType = typeof(Custom.Resource), Name="Profit Target", Order=3, GroupName="NinjaScriptStrategyParameters")]
 		public int ProfitTarget
 		{ get; set; }
+		
+		private double BigBuys
+		{
+			get{ return Whales.Buys[0]; }
+		}
+		
+		private double BigSells
+		{
+			get{ return Whales.Sells[0]; }
+		}
+		
+		private	double AllBuys 
+		{
+			get{ return AllFish.Buys[0]; }
+		}
+		
+		private	double AllSells 
+		{
+			get{ return AllFish.Sells[0]; }
+		}
+		
+		private double NormalBuys
+		{
+			get{ return AllBuys - BigBuys; }
+		}
+		
+		private double NormalSells
+		{
+			get{ return AllSells - BigSells; }
+		}
 		
 
 		#endregion
